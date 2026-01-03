21b2dca32675c95f673d4e4d7ee14b7d898dfe67
Adam Burucs <adam.burucs@gmail.com>
2026-01-03 19:40:24 +0100

    feat: add search format arg and enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Copyright 2026 Adam Burucs. Licensed under custom Source Available License
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 const VERSION_INFO: &str = concat!(
     env!("CARGO_PKG_VERSION"),
@@ -13,14 +13,30 @@ const VERSION_INFO: &str = concat!(
 #[derive(Parser)]
 #[command(version = VERSION_INFO)]
 #[command(
-    long_about = "This tool lists photo files in a specified directory. It supports raw files and compressed files.",
+    long_about = "This tool lists photo files in a specified directory.\nIt supports raw files and compressed files.\nRaw files include formats from Canon, Sony, Nikon, Fujifilm and Panasonic.",
     after_help = "Examples:\n  pfl ./photos          List photo files in the 'photos' directory\n  pfl ./photos list.txt   List photo files and save to 'list.txt'"
 )]
 struct Cli {
     /// The directory to list photo files from
     directory: std::path::PathBuf,
+    /// Specify the search format
+    #[arg(
+        short, 
+        long, 
+        value_enum, 
+        default_value_t = SearchFormat::All
+    )]
+    format: SearchFormat,
     /// Optional path to save the list
     path: Option<std::path::PathBuf>,
+}
+
+// Derived ValueEnum allows clap to treat these variants as valid CLI options
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum SearchFormat {
+    All,
+    Raw,
+    Compressed,
 }
 
 fn main() {
