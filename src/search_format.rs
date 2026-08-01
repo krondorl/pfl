bd9810095d7a720570a9bab50c187dcf34d954ea
Adam Burucs <adam.burucs@gmail.com>
2026-08-01 13:59:49 +0200

    feat: add copyright info

--- src/search_format.rs
+++ src/search_format.rs
@@ -1,3 +1,5 @@
+// Copyright 2026 Adam Burucs. Licensed under custom Source Available License
+
 use clap::ValueEnum;
 
 // Derived ValueEnum allows clap to treat these variants as valid CLI options
